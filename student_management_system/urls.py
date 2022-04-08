0a4bc5a9876b7f4e176f7757bbe2195f0257a45c
mehei17n <mehedi15-13997@diu.edu.bd>
2022-04-09 02:57:28 +0600

    login & logout

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -8,4 +8,13 @@ from .import views, Hod_Views, Staff_Views, Student_Views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', views.BASE, name='base'),
+
+    #Login Path
+    path('', views.LOGIN, name='login'),
+    path('doLogin', views.doLogin, name='doLogin'),
+    path('doLogout', views.doLogout, name='logout'),
+
+    #Hod Pamel
+    path('Hod/Home', Hod_Views.HOME, name='hod_home'),
+
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
